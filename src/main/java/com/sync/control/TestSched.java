2c3a631267f96ed61612dd677e772d58112b5589
yaoxiaohui <21062577@qq.com>
2017-09-27 14:14:44 +0800

    修改定时器

--- src/main/java/com/sync/control/TestSched.java
+++ src/main/java/com/sync/control/TestSched.java
@@ -2,11 +2,9 @@ package com.sync.control;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class TestSched {
-
 
     @Scheduled(cron="*/5 * * * * ?")
     public void run() {
